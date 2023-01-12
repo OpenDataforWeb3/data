1d5a74f332f128b70e1a7f6ff156b00b0fc38ad1
Zen <zengatsuu@gmail.com>
2023-01-12 22:41:06 +0100

    shore: add github pages deploy

--- next.config.js
+++ next.config.js
@@ -3,9 +3,24 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 });
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = '/'
+let basePath = ''
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 module.exports = {
   ...withNextra(),
   images: {
     unoptimized: true,
   },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
